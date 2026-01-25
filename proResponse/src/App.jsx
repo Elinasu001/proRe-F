907c90319dfa39af653702ec874d57ab4ed0a962
suelina001 <elinaen001@gmail.com>
2026-01-26 04:55:03 +0900

    feat: header 작업 [라우터]

--- proResponse/src/App.jsx
+++ proResponse/src/App.jsx
@@ -1,16 +1,35 @@
+
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import CommonGlobalStyles from './assets/styles/CommonGlobalStyles';
+import ChatRoom from './component/ChatRoom/ChatRoom.jsx';
+import Header from './component/Common/Header/Header.jsx';
+import Home from './component/Home/Home.jsx';
+import TestChatRooms from './component/TestChatRooms/TestChatRooms.jsx';
 
+// 임시 페이지 컴포넌트 (실제 구현시 교체)
+const Favorite = () => <div>찜 목록 페이지</div>;
+const MyPage = () => <div>마이페이지</div>;
+const Quote = () => <div>견적 요청 페이지</div>;
+
+function App() {
   return (
     <>
+      <CommonGlobalStyles />
+      <Header />
       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
-        <Route path='/expertTransaction' element={<h1>ExpertReponse</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/TestChatRooms" element={<TestChatRooms />} />
+        <Route path="/chatRoom/:id" element={<ChatRoom />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/myquote" element={<Quote />} />
+        <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
       </Routes>
-    </>   
-  )
+    </>
+  );
 }
 
 export default App
--- proResponse/src/main.jsx
+++ proResponse/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
-    
     <App />
   </BrowserRouter>
-)
+);
